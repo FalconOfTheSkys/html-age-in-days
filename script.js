9509ed0a78abf4b99aaa339619216fb3a897a5d4
Deus <87348669+FalconOfTheSkys@users.noreply.github.com>
2021-09-18 16:31:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+ageYears = prompt("Enter your age in years:");
+document.getElementById("age-in-years").innerHTML = "Age in years: "+ageYears;
+/*
+Formula:
+
+ageYears*365
+*/
+
+document.getElementById("age-in-days").innerHTML = "Age in days: "+ageYears*365;
+
